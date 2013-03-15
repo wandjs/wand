07b9b27290c74834bb7d53b51e0a0477fd7c7732
Jamie Copeland <jamie@jamiecopeland.com>
2013-03-15 15:11:59 +0000

    Stub templater and event bus

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var environment;
+
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
+
+define(
+  [
+    'underscore'
+  ],
+  function(
+    _
+  ){
+    
+    var wand = {};
+
+    return wand;
+});
+
