77b1815ff58358fb1a51f79112bf404d11df256b
Jamie Copeland <jamie@jamiecopeland.com>
2013-03-19 18:10:24 +0000

    Added broken addRawTemplates test

--- test/templater.js
+++ test/templater.js
@@ -2,11 +2,13 @@ var expect = require('chai').expect;
 var templater = require('../lib/templater');
 var handleBars = require('handleBars');
 
+// Dummy raw templates
 var innerTemplate = '<p>{{body}}</p>';
 var outerTemplate = '<hr/><div><h1>{{title}}</h1>{{{template "inner" message}}}</div><hr/>';
 var headerTemplate = '<h1>{{title}}</h1>';
 var footerTemplate = '<h3>{{emailAddress}}</h3>';
 
+// Dummy compiled templates
 var compiledHeaderTemplate = handleBars.compile(headerTemplate);
 var compiledFooterTemplate = handleBars.compile(footerTemplate);
 
@@ -29,7 +31,7 @@ describe('templater', function(){
     );
 
   });
-  
+
   describe('initialize', function(){
     
     it('should initialize with raw templates', function(){
@@ -42,6 +44,32 @@ describe('templater', function(){
       expect(templater.compiledTemplates).to.have.keys(['header', 'footer']);
       expect(templater.compiledTemplates.header).to.equal(compiledHeaderTemplate);
       expect(templater.compiledTemplates.footer).to.equal(compiledFooterTemplate);
+    });
+    
+  });
+
+  describe('addRawTemplates', function(){
+    
+    before(function(){
+      
+      
+
+    });
+    
+    it('should add a single template', function(){
+
+      
+      templater.addRawTemplates({
+        header: headerTemplate,
+        footer: footerTemplate
+      });
+
+      console.log('templater.rawTemplates.header: ' + templater.rawTemplates.header);
+      console.log('templater.rawTemplates.footer: ' + templater.rawTemplates.footer);
+
+      expect(templater.rawTemplates).to.have.keys(['header', 'footer']);
+      // expect(templater.rawTemplates.header).to.equal(headerTemplate);
+      // expect(templater.rawTemplates.footer).to.equal(footerTemplate);
     });
     
   });
