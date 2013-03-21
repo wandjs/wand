eaccbc963d790b33a5c12167efa4f5eab12e81be
Jamie Copeland <jamie@jamiecopeland.com>
2013-03-21 14:27:20 +0000

    Added more event bus tests

--- test/event-bus.js
+++ test/event-bus.js
@@ -13,36 +13,78 @@ var should = chai.should;
 
 describe('EventBus', function(){
 
-  // var simpleEventHandler = function(){};
+  var simpleArg = 'hello';
+  var complexArg = { message:'world' };
+  var callBack;
 
-  var obj = {
-    simpleEventHandler: function(){}
-  }
 
   beforeEach(function(){
-    // sinon.spy(simpleEventHandler);
+    callBack = sinon.spy();
   });
 
   describe('trigger', function(){
     
-    it('simple event handler should fire', function(done){
-      
-      var callBack = sinon.spy();
-      // var callBack = function(){
-      //   console.log('arguments: ', arguments);
-      // }
+    it('simple event handler should fire without arguments', function(done){
+
       eventBus.on('myEvent', callBack);
-      eventBus.trigger('myEvent', 'hello');
       
+      eventBus.trigger('myEvent', 'hello');
+
       expect(callBack).to.be.calledOnce;
-      expect(callBack).to.be.calledWithExactly('hello');
 
       done();
     });
-    
+
+    it('simple event handler should fire with arguments', function(done){
+
+      eventBus.on('myEvent', callBack);
+      
+      eventBus.trigger('myEvent', simpleArg, complexArg);
+      
+      expect(callBack).to.be.calledOnce;
+      expect(callBack).to.be.calledWithExactly(simpleArg, complexArg);
+
+      done();
+    });
+
+    it('complex event handler should fire without arguments', function(done){
+
+      var level0Callback = sinon.spy();
+      var level1Callback = sinon.spy();
+      var level2Callback = sinon.spy();
+
+      eventBus.on('outer', level0Callback);
+      eventBus.on('outer:inner', level1Callback);
+      eventBus.on('outer:inner:myEvent', level2Callback);
+
+      eventBus.trigger('outer:inner:myEvent');
+
+      expect(level0Callback).to.be.calledOnce;
+      expect(level1Callback).to.be.calledOnce;
+      expect(level2Callback).to.be.calledOnce;
+
+      done();
+    });
+
+    it('complex event handler should fire without arguments', function(done){
+
+      var level0Callback = sinon.spy();
+      var level1Callback = sinon.spy();
+      var level2Callback = sinon.spy();
+
+      eventBus.on('outer', level0Callback);
+      eventBus.on('outer:inner', level1Callback);
+      eventBus.on('outer:inner:myEvent', level2Callback);
+
+      eventBus.trigger('outer:inner:myEvent', simpleArg, complexArg);
+
+      expect(level0Callback).to.be.calledWith(simpleArg, complexArg);
+      expect(level1Callback).to.be.calledWith(simpleArg, complexArg);
+      expect(level2Callback).to.be.calledWith(simpleArg, complexArg);
+
+      done();
+    });
+
   });
-
-  
-
 
 });
