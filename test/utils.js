6d1c30365b68394e0c536cdb855d2f97a77b553a
Matt Sweetman <git@webroo.org>
2013-03-24 15:05:48 +0000

    Added utils.createClass() as syntatic sugar for creating class-like prototype chains. Also included tests.

--- test/utils.js
+++ test/utils.js
@@ -374,4 +374,113 @@ describe('utils', function(){
     });
   });
 
+  describe('createClass()', function(){
+    var createClass = utils.createClass;
+    it('should return a constructor function when no arguments are given', function(){
+      var Animal = createClass();
+      expect(Animal).to.be.a('function');
+    });
+    it('should return a constructor function with valid prototype when no arguments are given', function(){
+      var Animal = createClass();
+      expect(Animal.prototype).to.be.an('object');
+    });
+    it('should return a constructor function with valid prototype.constructor when no arguments are given', function(){
+      var Animal = createClass();
+      expect(Animal.prototype.constructor).to.be.a('function');
+      expect(Animal.prototype.constructor).to.equal(Animal);
+    });
+    it('should return the constructor function specified in the details', function(){
+      var details = {
+        constructor: function() {}
+      };
+      var Animal = createClass(details);
+      expect(Animal).to.be.a('function');
+      expect(Animal).to.equal(details.constructor);
+    });
+    it('should return a constructor function even if no constructor is specified in the details', function(){
+      var Animal = createClass({});
+      expect(Animal).to.be.a('function');
+    });
+    it('should copy detail properties onto the new constructor prototype', function(){
+      var Animal = createClass({
+        species: 'cat'
+      });
+      expect(Animal.prototype.species).to.equal('cat');
+    });
+    it('new instance should create an object whose prototype has the details properties', function(){
+      var Animal = createClass({
+        species: 'cat'
+      });
+      var cat = new Animal();
+      expect(Object.getPrototypeOf(cat).species).to.equal('cat');
+    });
+    it('new instance should create an object whose prototype.constructor is the original constructor', function(){
+      var details = {
+        constructor: function() {},
+        species: 'cat'
+      };
+      var Animal = createClass(details);
+      var cat = new Animal();
+      expect(Object.getPrototypeOf(cat).constructor).to.equal(details.constructor);
+    });
+    it('should return a valid constructor function for a subclass', function(){
+      var Animal = createClass({});
+      var Dog = createClass(Animal, {});
+      expect(Dog).to.be.an('function');
+    });
+    it('should return a valid constructor function for a subclass with specified constructor function', function(){
+      var Animal = createClass({});
+      var dogDetails = {
+        constructor: function() {}
+      };
+      var Dog = createClass(Animal, dogDetails);
+      expect(Dog).to.equal(dogDetails.constructor);
+    });
+    it('should create a subclass constructor function whose prototype is an instance of the superclass', function(){
+      var Animal = createClass({
+        species: 'unknown'
+      });
+      var Dog = createClass(Animal, {
+        species: 'dog'
+      });
+      expect(Object.getPrototypeOf(Dog.prototype)).to.be.an('object');
+      expect(Object.getPrototypeOf(Dog.prototype).species).to.equal('unknown');
+    });
+    it('new instance of subclass should create an object prototype has the details properties', function(){
+      var Animal = createClass({});
+      var Dog = createClass(Animal, {
+        species: 'dog'
+      });
+      var dog = new Dog();
+      expect(Object.getPrototypeOf(dog).species).to.equal('dog');
+    });
+    it('new instance of subclass should create an object whose prototype.constructor is the original constructor', function(){
+      var Animal = createClass({});
+      var dogDetails = {
+        constructor: function() {}
+      };
+      var Dog = createClass(Animal, dogDetails);
+      var dog = new Dog();
+      expect(Object.getPrototypeOf(dog).constructor).to.equal(dogDetails.constructor);
+    });
+    it('new instance of subclass should create an object whose __proto__._proto__ should have superclass details', function(){
+      var Animal = createClass({
+        species: 'unknown'
+      });
+      var Dog = createClass(Animal, {});
+      var dog = new Dog();
+      expect(Object.getPrototypeOf(Object.getPrototypeOf(dog)).species).to.equal('unknown');
+    });
+    it('new instance of subclass should create an object whose details override the superclass', function(){
+      var Animal = createClass({
+        species: 'unknown'
+      });
+      var Dog = createClass(Animal, {
+        species: 'dog'
+      });
+      var dog = new Dog();
+      expect((dog).species).to.equal('dog');
+    });
+  });
+
 });
