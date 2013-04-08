54905600b36d7f5187317b0874ef6408c91fb825
Jamie Copeland <jamie@jamiecopeland.com>
2013-04-08 16:41:52 +0100

    Moved storedOptions into AbstractLoader and added abstract-loader test

--- test/io/abstract-loader.js
+++ test/io/abstract-loader.js
@@ -2,13 +2,13 @@ var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require("sinon-chai");
 var $ = require("jquery");
-var DataLoader = require('../../lib/io/DataLoader');
+var AbstractLoader = require('../../lib/io/AbstractLoader');
 
 chai.use(sinonChai);
 
 var expect = chai.expect;
 
-describe('DataLoader', function(){
+describe('AbstractLoader', function(){
   var id1 = 'a';
   var pathString1 = 'data/data-1.json';
   var pathString2 = 'data/data-2.json';
@@ -21,27 +21,78 @@ describe('DataLoader', function(){
   var dataLoaderStringPath;
   var dataLoaderFunctionPath;
 
-  var server;
+  
 
   beforeEach(function() {
     callBack = sinon.spy();
     
-    dataLoader = new DataLoader( { id: id1, path: pathString1 } );
+    dataLoader = new AbstractLoader( { id: id1, path: pathString1 } );
     dataLoader.off();
-    dataLoaderStringPath = new DataLoader( { id: id1, path: pathString1 } );
+    dataLoaderStringPath = new AbstractLoader( { id: id1, path: pathString1 } );
     dataLoaderStringPath.off();
-    dataLoaderFunctionPath = new DataLoader( { id: id1, path: pathFunction } );
+    dataLoaderFunctionPath = new AbstractLoader( { id: id1, path: pathFunction } );
     dataLoaderFunctionPath.off();
-
-    sinon.stub($, 'ajax').yieldsTo('success', json1);
   });
 
   afterEach(function() {
-    $.ajax.restore();
+    // $.ajax.restore();
+  });
+
+  describe('initialize', function(){    
+
+    it('should have id property set', function(){
+      expect(dataLoader.id).to.equal(id1);
+    });
+
+    it('should have path set to a string', function(){
+      expect(dataLoaderStringPath.path).to.equal(pathString1);
+    });
+
+    it('should have path set to a function', function(){
+      expect(dataLoaderFunctionPath.path).to.equal(pathFunction);
+    });
+
+  });
+
+  describe('getPath', function(){
+
+    it('should return correct string for string type', function(){
+      expect(dataLoaderStringPath.getPath()).to.equal(pathString1);
+    });
+
+    it('should return correct string for function type', function(){
+      expect(dataLoaderFunctionPath.getPath()).to.equal(pathString2);
+    });
+
+  });
+
+  describe('triggerStart', function(){
+
+    it('should trigger start event', function(done){
+      dataLoader.on('start', callBack);
+      dataLoader.triggerStart();
+      expect(callBack).to.be.calledOnce;
+      expect(callBack).to.be.calledWithMatch({target:dataLoader});
+      done()
+    });
+
+  });
+
+  describe('triggerComplete', function(){
+
+    it('should trigger complete event', function(done){
+      dataLoader.on('complete', callBack);
+      dataLoader.triggerComplete();
+      expect(callBack).to.be.calledOnce;
+      expect(callBack).to.be.calledWithMatch({target:dataLoader});
+      done()
+    });
+
   });
 
   describe('start', function(){
-     it('should fire onStart', function(done){
+
+    it('should fire onStart', function(done){
       var onStartSpy = sinon.spy(dataLoader, 'onStart');
       dataLoader.start();
       expect(onStartSpy).to.be.calledOnce;
@@ -50,7 +101,7 @@ describe('DataLoader', function(){
 
     it('should allow custom onStart handler', function(done){
       var check = 0;
-      dataLoader = new DataLoader({
+      dataLoader = new AbstractLoader({
         id: id1,
         path: pathString1,
         onStart: function()
@@ -75,19 +126,6 @@ describe('DataLoader', function(){
       done();
     });
 
-    it('complete should be true', function(done){
-      expect(dataLoader.complete).to.be.false;
-      dataLoader.start();
-      expect(dataLoader.complete).to.be.true;
-      done();
-    });
-
-    it('data should be set', function(done){
-      dataLoader.start();
-      expect(dataLoader.data).to.be.equal(json1);
-      done();
-    });
-
     it('should fire onComplete', function(done){
       var onCompleteSpy = sinon.spy(dataLoader, 'onComplete');
       dataLoader.start();
@@ -95,28 +133,9 @@ describe('DataLoader', function(){
       done();
     });
 
-    it('should allow custom onStart handler', function(done){
-      var check = 0;
-      dataLoader = new DataLoader({
-        id: id1,
-        path: pathString1,
-        onStart: function()
-        {
-          check = 1;
-          this.triggerStart();
-        }
-      });
-
-      dataLoader.on('start', callBack);
-      dataLoader.start();
-      expect(callBack).to.be.calledOnce;
-      expect(check).to.equal(1);
-      done();
-    });
-
     it('should allow custom onComplete handler', function(done){
       var check = 0;
-      dataLoader = new DataLoader({
+      dataLoader = new AbstractLoader({
         id: id1,
         path: pathString1,
         onComplete: function()
@@ -141,6 +160,6 @@ describe('DataLoader', function(){
       done();
     });
 
-  });  
+  });
 
 });
