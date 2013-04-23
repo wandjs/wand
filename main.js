88d46dd82155d66a439d50c348158eb7a839a572
Matt Sweetman <git@webroo.org>
2013-04-23 11:51:47 +0100

    Main.js now returns an object containing all the modules

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-var environment;
-
-if (typeof define !== 'function') {
-  var define = require('amdefine')(module);
-}
-
-define(
-  [
-    'underscore'
-  ],
-  function(
-    _
-  ){
-    
-    var wand = {};
-
-    return wand;
+define(function(require, exports, module) {
+    return {
+      BaseClass: require('./lib/base-class'),
+      BaseView: require('./lib/base-view'),
+      EventBus: require('./lib/event-bus'),
+      EventDispatcher: require('./lib/event-dispatcher'),
+      Templater: require('./lib/templater'),
+      Utils: require('./lib/utils'),
+      controls: {
+        BaseControl: require('./lib/controls/base-control'),
+        Checkbox: require('./lib/controls/checkbox'),
+        DropDown: require('./lib/controls/drop-down'),
+        RadioButtonGroup: require('./lib/controls/radio-button-group'),
+        TextInput: require('./lib/controls/text-input')
+      },
+      io: {
+        AbstractLoader: require('./lib/io/AbstractLoader'),
+        DataLoader: require('./lib/io/DataLoader'),
+        ImageLoader: require('./lib/io/ImageLoader'),
+        LoadQueue: require('./lib/io/LoadQueue')
+      }
+    };
 });
-
